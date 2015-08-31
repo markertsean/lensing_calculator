da859c7438bdf8300e3eb96f28044238e0bb1237
Sean Markert <markertsean0@gmail.com>
2015-08-31 16:47:52 -0600

    Overloaded randVal to handle different input

--- include/my_utilities.h
+++ include/my_utilities.h
@@ -2,16 +2,28 @@
 #define MY_UTILITIES
 
 
-double chiSquared( 
+double chiSquared(
 		  double    *array1,  //One set of values to compare
 		  double    *array2,  //Set to compare against
 		  double  *errorArr,  //Errors to divide by
 		  int    N_elements); //Size of the arrays
 
 
-double    randVal( 
+double    randVal(
 		  double  low,  //Lower limit on randVal
 		  double high); //Upper limit on randVal
+
+double    randVal(
+		  int     low,  //Lower limit on randVal
+		  double high); //Upper limit on randVal
+
+double    randVal(
+		  double  low,  //Lower limit on randVal
+		  int    high); //Upper limit on randVal
+
+int       randVal(
+		  int     low,  //Lower limit on randVal
+		  int    high); //Upper limit on randVal
 
 
 #endif
--- src/myUtilities.cpp
+++ src/myUtilities.cpp
@@ -1,19 +1,65 @@
 #include <cstdlib>
 
 //Returns value of chi^2 fit, ignores points w/0 error
-double chiSquared( double *array1, double *array2, double *errorArr, int N_elements){
+double chiSquared(
+                    double    *array1 ,   // First  array to compare
+                    double    *array2 ,   // Second array to compare
+                    double  *errorArr ,   // Error values in measurement
+                    int    N_elements ){  // Number of elements in arrays, if different size might break
+
   //Chi^2 = sum ( predicted - data / error )^2
   double chi2(0), temp(0);
+
+
   for ( int i=0; i<N_elements; ++i ){
+
     if ( errorArr[i] != 0.0 ){             //If no error, ignore
+
       temp  = ( array1[i] - array2[i] ) / errorArr[i] ;
       chi2 += temp * temp;
+
     }
+
   }
+
+
   return chi2;
 }
 
 //Generates uniformly distribution between low and high
-double randVal( double low, double high ){
+double randVal(
+                double  low ,  //Lower limit of random number
+                double high ){ //Upper limit of random number
+
   return ((double) rand() / RAND_MAX) * (high - low) + low;
+
 }
+
+
+//Like randVal, but with integers
+double randVal(
+                int     low ,  //Lower limit of random number
+                double high ){ //Upper limit of random number
+
+  return randVal( (double) low, high);
+}
+
+
+//Like randVal, but with integers
+double randVal(
+                double  low ,  //Lower limit of random number
+                int    high ){ //Upper limit of random number
+
+  return randVal( low, (double) high);
+}
+
+
+
+//Like randVal, but with integers. This version returns an integer.
+int      randVal(
+                int     low ,  //Lower limit of random number
+                int    high ){ //Upper limit of random number
+
+  return (int) (randVal( (double) low, (double) high));
+}
+
